86e0e1847e50800aacfe21657795ef453ce21b47
Cody <codyli332@gmail.com>
2016-03-31 22:16:46 -0400

    Updated Enemy and Timer apis

--- src/game_engine/game_elements/Enemy.java
+++ src/game_engine/game_elements/Enemy.java
@@ -8,4 +8,16 @@ public interface Enemy {
 	*
 	*/
 	public void update();
+	
+	/*
+	* the Enemy fires a projectile at a target, which subsequently applies
+	* an Affector on it. 
+	*
+	*/
+	public void fire();
+	
+	/*
+	 * updates the Enemy's health when an Affector is applied to it. 
+	 */
+	public void takeDamage(double damage);
 }
--- src/game_engine/game_elements/Timer.java
+++ src/game_engine/game_elements/Timer.java
@@ -1,5 +1,22 @@
 package game_engine.game_elements;
 
+/*
+* External API interface that will be available to the authoring environment for extension and creation
+* of XML files for use in games. API specifies some basic functionality of timers and which methods need to 
+* be implemented for subclasses created in the authoring environment.
+*/
 public interface Timer {
-
+	
+	/*
+	 * Progresses the program by one time step and updates all
+	 * necessary properties dependent on time (position due to velocity,
+	 * affector duration). 
+	 */
+	public void nextTimeStep();
+	
+	/*
+	 * Returns the number of ticks since the program first executed.
+	 */
+	
+	public int getTicks(); 
 }
