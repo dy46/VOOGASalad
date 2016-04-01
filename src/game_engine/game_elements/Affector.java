75ad0571b359421d8bbdb4c88e468d0d7772fe92
Paul Cruz <pbc8@duke.edu>
2016-03-31 20:57:20 -0400

    Wrote some comments for API methods

--- src/game_engine/game_elements/Affector.java
+++ src/game_engine/game_elements/Affector.java
@@ -3,7 +3,17 @@ package game_engine.game_elements;
 import game_engine.properties.UnitProperties;
 
 public interface Affector {
-
+	/*
+	* Applies an effect to a unit by altering the 
+	* UnitProperties of a GameElement object. The effect is determined
+	* by the implementation of the method (this could involve)
+	* decrementing health, increasing/decreasing speed, etc.
+	* The overall effect is dependent on which properties are changed
+	*
+	* @param  properties  A UnitProperties object that represents the current state of the GameElement 
+	*
+	*
+	*/
 	public abstract void apply(UnitProperties properties);
 	
 }
--- src/game_engine/game_elements/GameElement.java
+++ src/game_engine/game_elements/GameElement.java
@@ -10,15 +10,35 @@ import game_engine.properties.UnitProperties;
  */
 
 public interface GameElement {
-	
+	/*
+	* Updates the GameElement (changes position, applies affectors, etc.)
+	*/
 	public abstract void update();
-	
+
+	/*
+	* returns a String identifier that can be used to connect backend and frontend components between
+	* the game player and the game engine. (This could be used to allow a player to select units, for example).
+	*/
 	public abstract String getID();
-	
+
+	/*
+	* Returns the UnitProperties object for use by other components of the game (mainly affectors)
+	*/
 	public abstract UnitProperties getProperties();
 	
+	/*
+	* method sets the String identifier for the GameElement object. This can be used to connect
+	* backend and frontend implementations of the game, so that users can select units on screen,
+	* and the appropriate backend classes will be updated.
+	*/
 	public abstract void setID(String ID);
-	
+	/*
+	* Initializes the properties of the GameElement 
+	* according to the values proposed in the inputs 
+	* to the method
+	*
+	* @param  properties  UnitProperties object that contains information that will be used to set properties
+	*/
 	public abstract void setProperties(UnitProperties properties);
 	
 }
