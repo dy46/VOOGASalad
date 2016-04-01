8080bd78eb45e1b66dbac4aa07065e6e2feaae06
Briguy52 <Briguy52@gmail.com>
2016-04-01 12:11:02 -0400

    HelpMenu: added action to open a public Gist

--- src/auth_environment/view/Menus/FileMenu.java
+++ src/auth_environment/view/Menus/FileMenu.java
@@ -2,6 +2,10 @@ package auth_environment.view.Menus;
 
 /**
  * Created by BrianLin on 4/1/16.
+ * 
+ * Team member responsible:
+ * 
+ * This drop down menu should allow the Developer to 1) Save 2) Load 
  */
 public interface FileMenu {
 }
--- src/auth_environment/view/Menus/HelpMenu.java
+++ src/auth_environment/view/Menus/HelpMenu.java
@@ -1,7 +1,45 @@
 package auth_environment.view.Menus;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.Pane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
 /**
  * Created by BrianLin on 4/1/16.
+ * 
+ * Team member responsible: 
+ * 
+ * This menu 
  */
-public interface HelpMenu {
+
+public class HelpMenu extends Menu {
+	
+	// TODO: ask team where to extract these
+	private static String helpURL = "https://gist.github.com/Briguy52/7adf8cfe508e73267bb46ce2aebe7b4a"; 
+	private static String helpText = "Help";
+	private static int helpWidth = 800;
+	private static int helpHeight = 600; 
+	
+	private WebView browser = new WebView();
+    private WebEngine webEngine = browser.getEngine();
+	
+	public HelpMenu() {
+		this.setText(this.helpText);
+		this.setOnAction(e -> openHelpWindow());
+	}
+	
+	private void openHelpWindow() {
+		Stage stage = new Stage();
+		Pane root = new Pane();
+        stage.setTitle(this.helpText);
+		webEngine.load(this.helpURL);
+		root.getChildren().add(browser);
+		int width = this.helpWidth; // NOTE: eventually use parseInt(<width from properties file>) 
+		int height = this.helpHeight;
+        stage.setScene(new Scene(root, width, height));
+        stage.show();
+	}
 }
