40eca1a77b5f856488a9a68a4efd37f31a6b0387
Paul Cruz <pbc8@duke.edu>
2016-03-31 21:13:29 -0400

    wrote some basic methods for Projectile API

--- src/game_engine/game_elements/Projectile.java
+++ src/game_engine/game_elements/Projectile.java
@@ -1,5 +1,19 @@
 package game_engine.game_elements;
-
+/*
+* Internal API used to represent projectiles fired by towers and/or enemies. 
+* 
+*/
 public interface Projectile {
+	/*
+	* Updates the position of the projectile
+	*/
+	public void update();
 
+	/*
+	* Applies any affectors that the projectile may have, which can include burns, freezes, and so forth
+	*
+	* @param  elem  specifies the GameElement that the effects are being applied to
+	* 
+ 	*/
+	public void applyEffects(GameElement elem);
 }
