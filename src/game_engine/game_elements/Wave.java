42ced1c737570b518e008d81f95fe9ab3ded56fe
Cody <codyli332@gmail.com>
2016-03-31 22:29:41 -0400

    Updated Wave API

--- src/game_engine/game_elements/Wave.java
+++ src/game_engine/game_elements/Wave.java
@@ -1,5 +1,14 @@
 package game_engine.game_elements;
 
 public interface Wave {
-
+	
+	/*
+	 * Returns the number of enemies left in this wave
+	 */
+	public int getEnemiesLeft(); 
+	
+	/*
+	 * Spawns an enemy at the spawn location of the level
+	 */
+	public void spawnEnemy();
 }
