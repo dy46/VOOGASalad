03c3f05410d9ca8ff6bf10c1700f200fa1266bc1
Cody <codyli332@gmail.com>
2016-03-31 22:56:49 -0400

    Updated Damage API

--- src/game_engine/properties/Damage.java
+++ src/game_engine/properties/Damage.java
@@ -1,5 +1,18 @@
 package game_engine.properties;
 
 public class Damage {
-
+	
+	private double myDamage;
+	
+	public Damage(double damage){
+		this.myDamage = damage; 
+	}
+	
+	public double getDamage(){
+		return myDamage; 
+	}
+	
+	public void setDamage(double damage){
+		myDamage = damage; 
+	}
 }
