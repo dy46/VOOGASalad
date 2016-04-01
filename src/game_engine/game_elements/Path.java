92083eb2a45c25c7311b727d256db6c467963aae
Paul Cruz <pbc8@duke.edu>
2016-03-31 21:09:52 -0400

    added comments to the Path API

--- src/game_engine/game_elements/Path.java
+++ src/game_engine/game_elements/Path.java
@@ -1,5 +1,15 @@
 package game_engine.game_elements;
-
+/*
+* Internal API that will be used in order to represent paths 
+* for enemy movements.
+*/
 public interface Path {
-
+	/*
+	* Gets the next position (point) in the path.
+	* This will be used in order to determine which direction 
+	* an Enemy needs to move in next.
+	*
+	* @return	The next Position in the list of Positions that represent the path being taken.
+	*/
+	public Position getNextPosition();
 }
