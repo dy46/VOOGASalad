fb3000e633958f03ad7e76c78265294886eae481
Briguy52 <Briguy52@gmail.com>
2016-04-02 10:35:28 -0400

    Testing: trying to figure out how to get specific GameElement type from each instance

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		System.out.println();
 		myView = new View(primaryStage);
 		myView.display();
 	}
