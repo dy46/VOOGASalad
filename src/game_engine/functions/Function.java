29cdb889720b74d0b0d7e41ffa1562cdd07ad6f5
Adam Tache <at201@duke.edu>
2016-04-03 05:04:10 -0400

    Functions have names

--- src/game_engine/functions/Function.java
+++ src/game_engine/functions/Function.java
@@ -3,6 +3,8 @@ package game_engine.functions;
 import java.util.ArrayList;
 import java.util.List;
 
+import game_engine.EngineWorkspace;
+
 /**
  * This class represents a discrete function based on any number of independent variable as arguments.
  * A function is made up of Terms (see Term.java), which are themselves made up of constants and variables.
@@ -12,18 +14,39 @@ import java.util.List;
 public class Function {
 	
 	private List<Term> myTerms;
+	private EngineWorkspace myWorkspace;
+	private String myName;
 	
-	public Function(String function){
+	public Function(String type, String strength, EngineWorkspace workspace){
 		myTerms = new ArrayList<>();
-		
+		this.myWorkspace = workspace;
+		myTerms = myWorkspace.getFunctionLibrary().getTerms(type, strength);
 	}
 	
-	public double evaluate(int time){
+	public Function(String name, List<Term> terms){
+		this.myName = name;
+		this.myTerms = terms;
+	}
+	
+	public Function(String equation, EngineWorkspace workspace){
+		myTerms = new ArrayList<>();
+		this.myWorkspace = workspace;
+	}
+	
+	public double evaluate(int index){
 		double evaluation = 0;
 		for(Term term : myTerms){
-			evaluation += term.evaluate(time);
+			evaluation += term.evaluate(index);
 		}
 		return evaluation;
+	}
+	
+	public String getName(){
+		return myName;
+	}
+	
+	public List<Term> getTerms(){
+		return myTerms;
 	}
 	
 }
