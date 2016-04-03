55cecdfde28b8f8d595838e4168b2ede12f99ef5
Briguy52 <Briguy52@gmail.com>
2016-04-03 16:00:14 -0400

    Settings.java: added music menu item

--- src/auth_environment/view/Menus/SettingsMenu.java
+++ src/auth_environment/view/Menus/SettingsMenu.java
@@ -1,8 +1,11 @@
 package auth_environment.view.Menus;
 
+import java.io.File;
 import java.util.ResourceBundle;
 
+import auth_environment.delegatesAndFactories.FileChooserDelegate;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 /**
  * Created by BrianLin on 4/1/16.
@@ -12,12 +15,23 @@ public class SettingsMenu extends Menu {
 	private static final String NAMES_PACKAGE = "auth_environment/properties/names";
 	private ResourceBundle myNamesBundle = ResourceBundle.getBundle(NAMES_PACKAGE);
 	
+	private FileChooserDelegate myFileChooser = new FileChooserDelegate(); 
+	
 	public SettingsMenu() {
-		this.setText(myNamesBundle.getString("settingsMenuLabel"));
 		this.init();
 	}
 	
 	private void init() {
-	
+		this.setText(myNamesBundle.getString("settingsMenuLabel"));
+		MenuItem musicItem = new MenuItem(myNamesBundle.getString("musicItemLabel"));
+		musicItem.setOnAction(e -> this.chooseMusic());
+		this.getItems().addAll(musicItem); 
 	}
+	
+	// TODO: only show .mp3 files for selection
+	private void chooseMusic() {
+		File file = myFileChooser.chooseFile();
+		System.out.println(file.getAbsolutePath());
+	}
+
 }
