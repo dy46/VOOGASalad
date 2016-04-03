799d64d0576d5ed7209191cc61f4a86022376977
Briguy52 <Briguy52@gmail.com>
2016-04-03 13:33:26 -0400

    Enhanceents: try adding loading animation to Browser

--- src/auth_environment/delegatesAndFactories/BrowserWindowDelegate.java
+++ src/auth_environment/delegatesAndFactories/BrowserWindowDelegate.java
@@ -1,12 +1,18 @@
 package auth_environment.delegatesAndFactories;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class BrowserWindowDelegate {
+	
+	private Pane loadingPane;
+	private ProgressBar loadProgress;
+	private Label progressText;
 	
 	private WebView browser = new WebView();
     private WebEngine webEngine = browser.getEngine();
@@ -24,4 +30,22 @@ public class BrowserWindowDelegate {
         stage.setScene(new Scene(root, width, height));
         stage.show();
 	}
+	
+	  private void showMainStage() {
+		    mainStage = new Stage(StageStyle.DECORATED);
+		    mainStage.setTitle("FX Experience");
+		    mainStage.setIconified(true);
+
+		    // create a WebView.
+		    webView = new WebView();
+		    webView.getEngine().load("http://fxexperience.com/");
+		    loadProgress.progressProperty().bind(webView.getEngine().getLoadWorker().workDoneProperty().divide(100));
+
+		    // layout the scene.
+		    Scene scene = new Scene(webView, 1000, 600);
+		    webView.prefWidthProperty().bind(scene.widthProperty());
+		    webView.prefHeightProperty().bind(scene.heightProperty());
+		    mainStage.setScene(scene);
+		    mainStage.show();
+		  }
 }
