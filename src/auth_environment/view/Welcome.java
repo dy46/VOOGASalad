9b44bd853ba2d648a81f14ab27a3868f8876f563
Briguy52 <Briguy52@gmail.com>
2016-04-06 20:36:25 -0400

    TabMenu: extracted constants

--- src/auth_environment/view/Welcome.java
+++ src/auth_environment/view/Welcome.java
@@ -1,5 +1,19 @@
 package auth_environment.view;
 
+
+/**
+ * Created by BrianLin on 4/6/16.
+ * 
+ * Team member responsible: Brian
+ * 
+ * First (interactive) screen displayed to the Developer. Asks for Game name. 
+ */
+
 public class Welcome {
+	
+	public Welcome() {
+	}
+	
+	
 
 }
