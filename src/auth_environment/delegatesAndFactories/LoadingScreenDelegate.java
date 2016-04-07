1ea51963b77cd28638b537257e5f339b4a6e9913
Briguy52 <Briguy52@gmail.com>
2016-04-06 21:46:36 -0400

    LoadingScreenDelegate.java: class created to abstract away from BrowserWindowDelegate.java

---
+++ src/auth_environment/delegatesAndFactories/LoadingScreenDelegate.java
@@ -0,0 +1,33 @@
+package auth_environment.delegatesAndFactories;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.Pane;
+
+import java.util.ResourceBundle;
+
+/**
+ * Created by BrianLin on 4/6/16.
+ *
+ * Team member responsible: Brian
+ *
+ * What it does: Creates a loading (splash) screen featuring a loading bar.
+ */
+public class LoadingScreenDelegate {
+
+    private static final String NAMES_PACKAGE = "auth_environment/properties/names";
+    private ResourceBundle myNamesBundle = ResourceBundle.getBundle(NAMES_PACKAGE);
+
+    private static final String DIMENSIONS_PACKAGE = "auth_environment/properties/dimensions";
+    private ResourceBundle myDimensionsBundle = ResourceBundle.getBundle(DIMENSIONS_PACKAGE);
+
+    private Pane loadingPane;
+    private ProgressBar loadProgress;
+    private Label progressText;
+
+    public LoadingScreenDelegate() {
+
+    }
+
+    
+}
