966a30e1e674e0051e28d7bd7d16d4b6b9bd6a7a
David Yang <dy46@duke.edu>
2016-04-07 22:22:21 -0400

    refactored to use relfection to add elements to UI

--- src/game_engine/affectors/Affector.java
+++ src/game_engine/affectors/Affector.java
@@ -13,7 +13,7 @@ public class Affector {
 	private int elapsedTime;
 	private List<Function> myFunctions;
 
-	/*
+	/**
 	 * Applies an effect to a unit by altering the 
 	 * UnitProperties of a GameElement object. The effect is determined
 	 * by the implementation of the method (this could involve)
--- src/game_player/PlayerMainTab.java
+++ src/game_player/PlayerMainTab.java
@@ -1,16 +1,25 @@
 package game_player;
 
+import java.util.Enumeration;
 import java.util.ResourceBundle;
 
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class PlayerMainTab implements IPlayerTab{
+	private static final String GUI_ELEMENTS = "GUIElements";
+	private static final String PACKAGE_NAME = "game_player.";
 	private static final int PANEL_PADDING = 10;
 	private Tab myTab;
 	private BorderPane myRoot;
 	private ResourceBundle myResources;
+	private ResourceBundle elementsResources;
+	
+	private VBox gameSection;
+	private VBox configurationPanel;
+	private HBox gamePanel;
 	
 	private IGUIObject gameCanvas;
 	private IGUIObject gameSpeed;
@@ -18,6 +27,7 @@ public class PlayerMainTab implements IPlayerTab{
 	
 	public PlayerMainTab(ResourceBundle r) {
 		myResources = r;
+		elementsResources = ResourceBundle.getBundle(GUI_ELEMENTS);
 	}
 	
 	@Override
@@ -26,9 +36,9 @@ public class PlayerMainTab implements IPlayerTab{
 		myTab = new Tab();
 		myRoot = new BorderPane();
 		
+		createUISections();
 		initializeElements();
-		addGameCanvas();
-		createConfigurationPanel();
+		placeUISections();
 		
 		myTab.setContent(myRoot);
 		return myTab;
@@ -36,24 +46,51 @@ public class PlayerMainTab implements IPlayerTab{
 	
 	private void initializeTab() {
 	}
-
+	
 	//TODO: Possibly use reflection/other techniques to initialize elements.
 	private void initializeElements() {
-		gameCanvas = new GameCanvas(myResources);
-		gameSpeed = new GameSpeedSlider(myResources);
-		playerSwitchGames = new PlayerSwitchGames(myResources);
+		IGUIObject newElement = null;
+		Enumeration<String> resourceKeys = elementsResources.getKeys();
+		while (resourceKeys.hasMoreElements()) {
+			String currentKey = resourceKeys.nextElement();
+			String[] keyAndPosition = elementsResources.getObject(currentKey).toString().split(",");
+			try {
+				newElement = (IGUIObject) Class.forName(PACKAGE_NAME + keyAndPosition[0].trim())
+						.getConstructor(ResourceBundle.class).newInstance(myResources);
+
+				placeElement(newElement, keyAndPosition[1].trim());
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
-	private void addGameCanvas() {
-		VBox gameSection = new VBox(PANEL_PADDING);
-		gameSection.getChildren().add(gameCanvas.createNode());
+	private void placeElement(IGUIObject element, String position) {
+		try{
+			getClass().getDeclaredMethod(position, IGUIObject.class).invoke(this, element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void createUISections() {
+		gameSection = new VBox(PANEL_PADDING);
+		configurationPanel = new VBox(PANEL_PADDING);
+		gamePanel = new HBox(PANEL_PADDING);
+	}
+	
+	private void placeUISections() {
 		myRoot.setCenter(gameSection);
+		myRoot.setRight(configurationPanel);
+		myRoot.setBottom(gamePanel);
 	}
 	
-	private void createConfigurationPanel() {
-		VBox configurationPanel = new VBox(PANEL_PADDING);
-		configurationPanel.getChildren().add(gameSpeed.createNode());
-		configurationPanel.getChildren().add(playerSwitchGames.createNode());
-		myRoot.setRight(configurationPanel);
+	private void addToGameCanvas(IGUIObject element) {
+		gameSection.getChildren().add(element.createNode());
+	}
+	
+	private void addToConfigurationPanel(IGUIObject element) {
+		configurationPanel.getChildren().add(element.createNode());
 	}
 }
