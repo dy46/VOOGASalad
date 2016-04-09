3a81627ae2a267b2261016e2317dca3ecc8d2460
Briguy52 <Briguy52@gmail.com>
2016-04-09 17:15:03 -0400

    MapDisplay.java: extracted dimension constants

--- src/auth_environment/view/MapDisplay.java
+++ src/auth_environment/view/MapDisplay.java
@@ -1,5 +1,7 @@
 package auth_environment.view;
 
+import java.util.ResourceBundle;
+
 import auth_environment.backend.MapDisplayModel;
 import game_engine.game_elements.GameElement;
 import javafx.scene.layout.Pane;
@@ -8,26 +10,28 @@ import javafx.scene.layout.StackPane;
 /**
  * Created by BrianLin on 3/31/16.
  * Team member responsible: Xander
+ * Refactorings + Packaging: Brian
  *
  * This class is for the main Map Display showing the current Map.
  * The Developer should be able to place Game Elements (as long as they are placeable).
  */
 
 public class MapDisplay extends Pane {
-	 
-	// TODO: extract these values
-	private static double mapWidth = 400;
-	private static double mapHeight = 400; 
-	private static int numX = 10;
-	private static int numY = 10;
 	
+	private static final String DIMENSIONS_PACKAGE = "auth_environment/properties/dimensions";
+	private ResourceBundle myDimensionsBundle = ResourceBundle.getBundle(DIMENSIONS_PACKAGE);
+	 
 	private MapDisplayModel myModel;
 	private Grid myGrid;
 	
 	public MapDisplay() {
-		this.setPrefSize(this.mapWidth, this.mapHeight);
-		myModel = new MapDisplayModel(numX, numY);
-		myGrid = new Grid(myModel, mapWidth, mapHeight);
+		this.setPrefSize(Double.parseDouble(myDimensionsBundle.getString("defaultMapWidthPixels")), 
+						 Double.parseDouble(myDimensionsBundle.getString("defaultMapHeightPixels")));
+		myModel = new MapDisplayModel(Integer.parseInt(myDimensionsBundle.getString("defaultMapWidthCount")), 
+									  Integer.parseInt(myDimensionsBundle.getString("defaultMapHeightCount")));
+		myGrid = new Grid(myModel, 
+						  Double.parseDouble(myDimensionsBundle.getString("defaultMapWidthPixels")), 
+						  Double.parseDouble(myDimensionsBundle.getString("defaultMapWidthPixels")));
 		this.getChildren().add(myGrid.getPane());
 	}
 	
