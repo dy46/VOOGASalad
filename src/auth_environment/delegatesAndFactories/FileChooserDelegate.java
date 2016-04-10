8c3bf02223fd55e95fac5f23d6c5deb9fdcfa61a
Briguy52 <Briguy52@gmail.com>
2016-04-09 23:28:21 -0400

    Updated FileChooser delegate with chooseImage() method

--- src/auth_environment/delegatesAndFactories/FileChooserDelegate.java
+++ src/auth_environment/delegatesAndFactories/FileChooserDelegate.java
@@ -3,8 +3,10 @@ package auth_environment.delegatesAndFactories;
 import java.io.File;
 import java.util.ResourceBundle;
 
+import javafx.scene.control.ContextMenu;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class FileChooserDelegate {
 	
@@ -16,11 +18,30 @@ public class FileChooserDelegate {
 	}
 	
     public File chooseFile() {
-    	Stage stage = new Stage(); 
-        FileChooser f = new FileChooser();
+		return this.fileFromWindow(this.initChooser());
+    }
+    
+    private File fileFromWindow(FileChooser f) {
+        ContextMenu prefWindow = new ContextMenu();
+        File file = f.showOpenDialog(prefWindow.getOwnerWindow());
+        return file;
+    }
+    
+    private FileChooser initChooser() {
+    	FileChooser f = new FileChooser();
         f.setTitle(myNamesBundle.getString("fileChooserTitle")); 
-        File selectedFile = f.showOpenDialog(stage);
-        return selectedFile;
+        return f; 
+    }
+    
+    public File chooseImage() {
+    	return this.fileFromWindow(this.addImageFilter(this.initChooser()));
+    	
+    }
+    
+    private FileChooser addImageFilter(FileChooser f) {
+    	// TODO: do these need to be extracted? 
+    	f.getExtensionFilters().add(new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+    	return f;
     }
 
 }
