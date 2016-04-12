be6d29a5f5024fdb9c218d4d9d4df335f7b0fcf7
Briguy52 <Briguy52@gmail.com>
2016-04-11 23:31:06 -0400

    DragDelegate.java: class created

---
+++ src/auth_environment/delegatesAndFactories/DragDelegate.java
@@ -0,0 +1,12 @@
+package auth_environment.delegatesAndFactories;
+
+/**
+ * Created by BrianLin on 4/11/2016
+ * Team member responsible: Brian
+ *
+ * This delegate class adds dragging functions. Woowee dongerino. 
+ */
+
+public class DragDelegate {
+
+}
