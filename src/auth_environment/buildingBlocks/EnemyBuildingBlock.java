9d563dcec5284804835a94035323f238eb41037d
Cody <codyli332@gmail.com>
2016-04-13 12:26:26 -0400

    stashing

--- src/auth_environment/buildingBlocks/EnemyBuildingBlock.java
+++ src/auth_environment/buildingBlocks/EnemyBuildingBlock.java
@@ -1,5 +1,21 @@
 package auth_environment.buildingBlocks;
 
-public class EnemyBuildingBlock {
+import java.util.List;
+import game_engine.properties.*;
+import javafx.scene.image.ImageView; 
 
+public class EnemyBuildingBlock extends BuildingBlock{
+	
+	private Price myPrice;
+
+	public EnemyBuildingBlock(ImageView image, String name, double health, double damage, String teamName, double speed,
+			double direction, double price, List<Position> positionList, State s) {
+		super(image, name, health, damage, teamName, speed, direction, positionList, s);
+		myPrice = new Price(price); 
+
+	}
+	
+	
+	
+	
 }
