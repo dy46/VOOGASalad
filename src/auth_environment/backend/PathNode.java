f13cc2b4d21e490822da96056661d3a7b1710d84
Adam Tache <at201@duke.edu>
2016-04-13 00:58:33 -0400

    Post-order traversal and reflection for extracting Units from trees

--- src/auth_environment/backend/PathNode.java
+++ src/auth_environment/backend/PathNode.java
@@ -1,22 +1,26 @@
 package auth_environment.backend;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import game_engine.game_elements.Enemy;
 import game_engine.game_elements.Terrain;
+import game_engine.game_elements.Unit;
 import game_engine.properties.Position;
 
 public class PathNode {
 
 	private List<Position> myPositions;
 	private List<Terrain> myTerrains;
-	private List<Enemy> myEnemies;
+	private List<Enemy> myEnemys;
 	private PathNode myParent;
 	private List<PathNode> myChildren;
+	private String myID;
 
-	public PathNode(PathNode parent, Position startingPos){
+	public PathNode(PathNode parent, Position startingPos, String ID){
+		this.myID = ID;
 		this.myParent = parent;
 		myPositions = Arrays.asList(startingPos);
 		myTerrains = new ArrayList<>();
@@ -24,21 +28,22 @@ public class PathNode {
 	}
 
 	public PathNode() {
+		this.myID = "0";
 		this.myTerrains = new ArrayList<>();
 		this.myPositions = new ArrayList<>();
-		this.myEnemies = new ArrayList<>();
+		this.myEnemys = new ArrayList<>();
 		this.myParent = null;
 		this.myChildren = new ArrayList<>();
 	}
 
 	public void addEnemy(Enemy enemy){
-		this.myEnemies.add(enemy);
+		this.myEnemys.add(enemy);
 	}
-	
+
 	public void addTerrain(Terrain terrain){
 		this.myTerrains.add(terrain);
 	}
-	
+
 	public void addChild(PathNode child){
 		this.myChildren.add(child);
 	}
@@ -52,7 +57,7 @@ public class PathNode {
 	}
 
 	public List<Enemy> getEnemies(){
-		return myEnemies;
+		return myEnemys;
 	}
 
 	public PathNode getParent(){
@@ -63,4 +68,30 @@ public class PathNode {
 		return myChildren;
 	}
 
+	public String getID(){
+		return myID;
+	}
+
+	public List<Unit> getUnitsByType(Unit type){
+		String className = type.getClass().getSimpleName();
+		String instanceVarName = "my" + className + "s";
+		Field f = null;
+		try {
+			f = getClass().getDeclaredField(instanceVarName);
+		}
+		catch (NoSuchFieldException | SecurityException e1) {
+			// TODO: womp exception
+			e1.printStackTrace();
+		}
+		f.setAccessible(true);
+		List<Unit> listInstanceVar = null;
+		try {
+			listInstanceVar = (List<Unit>) f.get(this);
+		}
+		catch (IllegalArgumentException | IllegalAccessException e) {
+			// TODO: womp exception
+			e.printStackTrace();
+		}
+		return listInstanceVar;
+	}
 }
--- src/auth_environment/backend/PathTree.java
+++ src/auth_environment/backend/PathTree.java
@@ -1,10 +1,15 @@
 package auth_environment.backend;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
+import game_engine.affectors.Affector;
 import game_engine.game_elements.Enemy;
 import game_engine.game_elements.Path;
 import game_engine.game_elements.Terrain;
+import game_engine.game_elements.Unit;
 
 public class PathTree {
 
@@ -22,16 +27,26 @@ public class PathTree {
 		return myRoot;
 	}
 	
-	public List<Enemy> getEnemies(){
-		return null;
+	public List<Unit> getEnemies(){
+		return getUnits(myRoot, new Enemy("0", new ArrayList<Affector>(), 0));
 	}
 	
-	public List<Path> getPaths(){
-		return null;
+	public List<Unit> getPaths(){
+		return getUnits(myRoot, new Path("0"));
 	}
 	
-	public List<Terrain> getTerrains(){
-		return null;
+	public List<Unit> getTerrains(){
+		return getUnits(myRoot, new Terrain("0", 0));
+	}
+	
+	// Uses post-order traversal to extract List of appropriate Unit
+	private List<Unit> getUnits(PathNode node, Unit target){
+		List<Unit> myUnits = new ArrayList<>();
+		for(PathNode n : node.getChildren()){
+			myUnits.addAll(n.getUnitsByType(target));
+			myUnits.addAll(getUnits(n, target));
+		}
+		return myUnits;
 	}
 	
 }
