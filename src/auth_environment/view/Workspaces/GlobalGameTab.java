d5a597c3ecbe9958ad18ec10aa5ba801d80d5875
Briguy52 <Briguy52@gmail.com>
2016-04-18 18:19:06 -0400

    Rebuilt GameTab

--- src/auth_environment/view/Workspaces/GlobalGameTab.java
+++ src/auth_environment/view/Workspaces/GlobalGameTab.java
@@ -20,6 +20,8 @@ import javafx.scene.Node;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * Created by BrianLin on 3/31/16.
@@ -32,14 +34,21 @@ public class GlobalGameTab implements IWorkspace {
 
 	private static final String DIMENSIONS_PACKAGE = "auth_environment/properties/dimensions";
 	private ResourceBundle myDimensionsBundle = ResourceBundle.getBundle(DIMENSIONS_PACKAGE);
-
-	private BorderPane myBorderPane = new BorderPane(); 
+	
+	private static final String NAMES_PACKAGE = "auth_environment/properties/names";
+	private ResourceBundle myNamesBundle = ResourceBundle.getBundle(NAMES_PACKAGE);
+	
+	private static final String URLS_PACKAGE = "auth_environment/properties/urls";
+	private ResourceBundle myURLSBundle = ResourceBundle.getBundle(URLS_PACKAGE);
 
 	// TODO: replace with EngineWorkspace class
 	private GameData gameData; // This originates here via XML 
 //	private GameDataController myGameDataController; // TODO: find this
 	private NodeFactory myNodeFactory = new NodeFactory(); 
-
+	
+	private BorderPane myBorderPane = new BorderPane(); 
+	private TextField myGameNameField;
+	
 	public GlobalGameTab() {
 		this.gameData = new GameData();
 //		myPicker = new ElementPicker();
@@ -48,23 +57,31 @@ public class GlobalGameTab implements IWorkspace {
 	}
 
 	private void setupBorderPane() {
+		
 		this.myBorderPane.setPrefSize(Double.parseDouble(myDimensionsBundle.getString("defaultBorderPaneWidth")),
 				Double.parseDouble(myDimensionsBundle.getString("defaultBorderPaneHeight")));
 		
+		this.myBorderPane.setCenter(this.buildCenter());
 		
-		
-//		this.myBorderPane.setTop(new MenuToolBar(this.myTabPane, this.myPicker, this.gameData.getSettings(), myGameDataController));
-//		myPicker.setPrefSize(Integer.parseInt(myDimensionsBundle.getString("defaultPickerWidth")),
-//				Integer.parseInt(myDimensionsBundle.getString("defaultPickerHeight"))); 
-//		this.myBorderPane.setRight(myPicker);
-//		this.myBorderPane.setCenter(myDisplay);
 	}
 	
-//	private TextField buildTextInput() {
-//		this.gameNameInput = myNodeFactory.buildTextFieldWithPrompt(myNamesBundle.getString("gameNamePrompt"));
-//		this.gameNameInput.setOnAction(e -> this.submitButtonPressed());
-//		return this.gameNameInput; 
-//	}
+	private Node buildCenter() {
+		VBox center = myNodeFactory.buildVBox(Double.parseDouble(myDimensionsBundle.getString("defaultVBoxSpacing")), 
+				Double.parseDouble(myDimensionsBundle.getString("defaultVBoxPadding")));
+		center.getChildren().addAll(this.buildWompImage(),
+				myNodeFactory.centerNode(this.buildTextInput()));
+		return center; 
+	}
+	
+	private HBox buildWompImage() {
+		return myNodeFactory.centerNode(myNodeFactory.buildImageView(myNamesBundle.getString("wompWelcomeImage")));
+	}
+	
+	private TextField buildTextInput() {
+		this.myGameNameField = myNodeFactory.buildTextFieldWithPrompt(myNamesBundle.getString("gameNamePrompt"));
+		this.myGameNameField.setOnAction(e -> this.submitButtonPressed(this.myGameNameField));
+		return this.myGameNameField; 
+	}
 
 	//	public void writeToGameData() {
 	////		gameData.setLevels(myPicker.getLevels());
@@ -73,7 +90,19 @@ public class GlobalGameTab implements IWorkspace {
 	//		gameData.setTowerTypes(myPicker.getTowers());
 	//		gameData.setPaths(myDisplay.getGrid().getPathGraphFactory().getPaths());
 	//	}
-
+	
+	// TODO: exctract these methods to the GlobalGameModel class
+	private void submitButtonPressed(TextField input) {
+		if (checkValidInput(input)) {
+			this.gameData.getSettings().setName(input.getText());
+			input.clear();
+		}
+	}
+	
+	private boolean checkValidInput(TextField input) {
+		return input.getText().length() > 0; 
+	}
+	
 	@Override
 	public Node getRoot() {
 		return this.myBorderPane; 
