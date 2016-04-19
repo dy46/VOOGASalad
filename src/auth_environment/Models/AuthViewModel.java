a2085ab6b80d04b25abd8c14add1ba4c7cf0a812
Briguy52 <Briguy52@gmail.com>
2016-04-19 16:43:52 -0400

    IAuthViewModel.java and AuthViewModel.java classes created

---
+++ src/auth_environment/Models/AuthViewModel.java
@@ -0,0 +1,25 @@
+package auth_environment.Models;
+
+/**
+ * Created by BrianLin on 4/19/16
+ * Team member responsible: Brian
+ *
+ * This class holds the highest level of Auth Environment backend data. Most important is a single instance
+ * of IEngineWorkspace.java (all of our data). 
+ * 
+ * There will also be some helper methods- such as saving or loading the data. 
+ */
+
+public class AuthViewModel implements IAuthViewModel {
+	
+	public AuthViewModel() {
+		
+	}
+
+	@Override
+	public void saveToFile() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/auth_environment/Models/IAuthViewModel.java
@@ -0,0 +1,19 @@
+package auth_environment.Models;
+
+/**
+ * Created by BrianLin on 4/19/16
+ * Team member responsible: Brian
+ *
+ * This class holds the highest level of Auth Environment backend data. Most important is a single instance
+ * of IEngineWorkspace.java (all of our data). 
+ * 
+ * There will also be some helper methods- such as saving or loading the data. 
+ */
+
+// TODO: add to API changes document
+
+public interface IAuthViewModel {
+	
+	public void saveToFile(); 
+	
+}
