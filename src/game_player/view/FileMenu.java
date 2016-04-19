79cac5edce97980be5636c8a22b1be512e618764
David Yang <dy46@duke.edu>
2016-04-19 01:00:18 -0400

    readded create tab button and change menu interface to superclass

--- src/game_player/view/FileMenu.java
+++ src/game_player/view/FileMenu.java
@@ -6,41 +6,22 @@ import javafx.scene.control.Menu;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class FileMenu implements IMenuInterface{
+public class FileMenu extends PlayerMenu{
 
 	private static final int WINDOW_SIZE = 500;
 	private Stage menuStage;
 	private ResourceBundle myResources;
 	private MenuMaker menuMaker;
 	private Menu myMenu;
-	private GridPane myRoot;
-	private SwitchWindow mySwitchWindow;
-	private IGameView myView;
 	
 	public FileMenu(ResourceBundle r, IGameView view) {
+		super(r, view);
 		myResources = r;
 		menuMaker = new MenuMaker(r);
-		mySwitchWindow = new SwitchWindow(r);
-		myView = view;
 	}
 	
 	public Menu createMenu() {
-		myMenu = menuMaker.addMenu(myResources.getString("OptionsMenu"));
-		String[] menuItems = myResources.getString("Options").split("/");
-		for (String pair: menuItems) {
-			String[] combo = pair.split(",");
-			try {
-                menuMaker.addMenuItem(combo[0], e -> {
-                    try {
-                        getClass().getDeclaredMethod(combo[1]).invoke(this);
-                    } catch (Exception e2) {
-                        e2.printStackTrace();
-                    }
-                } , myMenu);
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-		}
+		myMenu = super.createNewMenu("FileMenu");
 		return myMenu;
 	}
 }
