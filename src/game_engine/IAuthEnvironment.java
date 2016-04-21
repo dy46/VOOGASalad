a6ecafb42ab192097a5bb34e715084b9af83b5ca
Briguy52 <Briguy52@gmail.com>
2016-04-20 22:55:00 -0400

    IAuthEnvironment: removed unneeded Unit creations methods

--- src/game_engine/IAuthEnvironment.java
+++ src/game_engine/IAuthEnvironment.java
@@ -30,44 +30,36 @@ public interface IAuthEnvironment {
 	
 	public void addLevel(Level level); 
 	
-	// These Units have been placed on the Map. Not to be confused with available Enemy and Tower Units. 
+	// These Units have been placed on the Map. Not to be confused with available Enemy and Tower Units.
+	
+	public void placeUnit(Unit unit); // add this Unit to current List of placed Units 
+	
+	public void setPlacedUnits(List<Unit> units); 
 	
 	public List<Unit> getPlacedUnits(); 
-
-	public void placeUnit(Unit unit); // add this Unit to current List of placed Units 
 	
 	// Austin will also use getTowers(), getTerrains(), and getEnemies() (seen below) 
 	
 	// Create Units + Affector Tab - Virginia
 	
-	public void addTower(Unit tower);
+	public void setTowers(List<Unit> towers); 
 	
 	public List<Unit> getTowers(); 
 	
-	public void clearTowers(); 
-	
-	public void addTerrain(Unit terrain);
+	public void setTerrains(List<Unit> terrain); 
 	
 	public List<Unit> getTerrains(); 
 	
-	public void clearTerrains(); 
-	
-	public void addEnemy(Unit enemy);
+	public void setEnemies(List<Unit> enemies); 
 	
 	public List<Unit> getEnemies(); 
 	
-	public void clearEnemies();
-	
-	public void addProjectile(Unit projectile); 
+	public void setProjectiles(List<Unit> projectiles); 
 	
     public List<Unit> getProjectiles();
     
-    public void clearProjectiles(); 
-    
-    public void addAffector(Affector affector); 
+    public void setAffectors(List<Affector> affectors); 
     
     public List<Affector> getAffectors();
-    
-    public void clearAffectors(); 
     
 }
