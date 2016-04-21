6bbd72efbd350b91c42adc43ec98439f252248f8
Adam Tache <at201@duke.edu>
2016-04-20 22:44:17 -0400

    Level and Wave factories and libraries

---
+++ src/game_engine/factories/WaveFactory.java
@@ -0,0 +1,29 @@
+package game_engine.factories;
+
+import java.util.List;
+
+import game_engine.game_elements.Unit;
+import game_engine.game_elements.Wave;
+import game_engine.libraries.WaveLibrary;
+
+public class WaveFactory {
+	
+	private WaveLibrary myWaveLibrary;
+
+	public WaveFactory(){
+		myWaveLibrary = new WaveLibrary();
+	}
+	
+	public void createWave(String name, List<Unit> enemies, List<Integer> spawnTimes){
+		myWaveLibrary.addWave(new Wave(name, enemies, spawnTimes));
+	}
+	
+	public void addEnemyToWave(String waveName, Unit enemy, int spawnTime){
+		myWaveLibrary.addUnitToWave(waveName, enemy, spawnTime);
+	}
+	
+	public WaveLibrary getWaveLibrary(){
+		return myWaveLibrary;
+	}
+	
+}
