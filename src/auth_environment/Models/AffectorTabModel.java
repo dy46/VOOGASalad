512856851815e5aa547196edf7bddc541f2bea4e
Briguy52 <Briguy52@gmail.com>
2016-04-24 19:14:13 -0400

    AffectorTabModel.java: interface created, methods created + filled

--- src/auth_environment/Models/AffectorTabModel.java
+++ src/auth_environment/Models/AffectorTabModel.java
@@ -1,9 +1,21 @@
 package auth_environment.Models;
 
-public class AffectorTabModel {
+import auth_environment.IAuthEnvironment;
+import auth_environment.Models.Interfaces.IAffectorTabModel;
+import game_engine.factories.AffectorFactory;
+
+public class AffectorTabModel implements IAffectorTabModel {
 	
-	public AffectorTabModel() {
-		
+	private AffectorFactory myAffectorFactory; 
+	
+	public AffectorTabModel(IAuthEnvironment auth) {
+		this.myAffectorFactory = auth.getAffectorFactory();
+		this.myAffectorFactory.setDefaultAffectors(auth.getFunctionFactory());
+	}
+
+	@Override
+	public AffectorFactory getAffectorFactory() {
+		return this.myAffectorFactory;
 	}
 
 }
--- src/auth_environment/Models/Interfaces/IAffectorTabModel.java
+++ src/auth_environment/Models/Interfaces/IAffectorTabModel.java
@@ -1,5 +1,9 @@
 package auth_environment.Models.Interfaces;
 
+import game_engine.factories.AffectorFactory;
+
 public interface IAffectorTabModel {
+	
+	public AffectorFactory getAffectorFactory(); 
 
 }
