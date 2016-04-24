25034faf7eee541bb78cf794b852a68e0dd78128
Paul Cruz <pbc8@duke.edu>
2016-04-24 14:27:54 -0400

    added in methods to remove and add placeable and spawning units

--- src/auth_environment/Models/WaveOverviewTabModel.java
+++ src/auth_environment/Models/WaveOverviewTabModel.java
@@ -11,9 +11,9 @@ import game_engine.game_elements.Wave;
 import game_engine.libraries.UnitLibrary;
 
 public class WaveOverviewTabModel implements IWaveOverviewTabModel{
+	
 	private Wave myWave;
 	private UnitLibrary myLibrary;
-	
 	public WaveOverviewTabModel(UnitLibrary lib, String name, int spawnTime){
 		this.myWave = new Wave(name, spawnTime);
 		this.myLibrary = lib;
@@ -32,17 +32,47 @@ public class WaveOverviewTabModel implements IWaveOverviewTabModel{
 	public List<String> getEnemyInfo() {
 		List<String> ret = new ArrayList<String>();
 		for(Unit u : this.myWave.getSpawningUnits()){
-			
+			ret.add(u.getName()); // TODO: maybe some other statistics?
 		}
 		return ret;
 	}
 
 	@Override
-	public void addSpwaningUnits(List<String> names, List<Integer> times) {
+	public void addSpawningUnits(List<String> names, List<Integer> times) {
 		for(int i = 0;i < names.size();i++){
 			this.addSpawningUnit(names.get(i), times.get(i));
 		}
 	}
+
+	@Override
+	public void removeSpawningUnit(int index) {
+		this.myWave.removeSpawningUnit(index);
+	}
+
+	@Override
+	public void addSpawningUnit(String name) {
+		this.addSpawningUnit(name, DEFAULT_TIME);
+	}
+
+	@Override
+	public void addPlacingUnit(String name) {
+		Unit u = this.myLibrary.getUnitByName(name);
+		if(u != null){
+			this.myWave.addPlacingUnit(u);
+		}
+	}
+
+	@Override
+	public void addPlacingUnits(List<String> names) {
+		names.forEach(name -> this.addPlacingUnit(name));
+	}
+
+	@Override
+	public void removePlacingUnit(int index) {
+		this.myWave.removePlacingUnit(index);
+	}
+	
+	
 	
 	
 	
