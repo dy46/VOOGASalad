293c53a912b84420f9b372dd078bc8edaafdff17
Briguy52 <Briguy52@gmail.com>
2016-04-24 00:14:43 -0400

    (Mainly) Paul + Brian implemented loading series of files for a unit animation

--- src/auth_environment/Models/AnimationTabModel.java
+++ src/auth_environment/Models/AnimationTabModel.java
@@ -1,6 +1,8 @@
 package auth_environment.Models;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -9,7 +11,7 @@ import auth_environment.backend.StateImageSaver;
 import game_engine.game_elements.Unit;
 
 public class AnimationTabModel implements IAnimationTabModel {
-	
+	private static final String SUFFIX = ".png";
 	private static final String URLS_PACKAGE = "auth_environment/properties/urls";
 	private ResourceBundle myURLSBundle = ResourceBundle.getBundle(URLS_PACKAGE);
 	
@@ -20,11 +22,19 @@ public class AnimationTabModel implements IAnimationTabModel {
 	private StateImageSaver mySaver;
 	
 	public AnimationTabModel(Unit unit) {
-		this.mySaver = new StateImageSaver(this.myURLSBundle.getString("saveImageLocation")); 
+		this.mySaver = new StateImageSaver(this.myURLSBundle.getString("saveImageLocation"), SUFFIX); 
+		this.myFiles = new ArrayList<File>();
+		this.myUnit = unit;
 	}
 
 	@Override
 	public void addFile(File file) {
 		this.myFiles.add(file);
+	}
+	
+	public void saveFiles(){
+		System.out.println(this.myUnit.getType());
+		System.out.println(this.myUnit.getName());
+		this.mySaver.saveFiles(this.myUnit.getType(), this.myUnit.getName(), this.myFiles);
 	}
 }
