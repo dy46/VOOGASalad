360a00964dd3fafc4af4895cdb1ec12fe98551e8
Briguy52 <Briguy52@gmail.com>
2016-04-25 17:09:19 -0400

    Try make LevelOverviewTab not extend Tab

--- src/auth_environment/view/tabs/LevelOverviewTab.java
+++ src/auth_environment/view/tabs/LevelOverviewTab.java
@@ -1,50 +1,18 @@
 package auth_environment.view.tabs;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import auth_environment.IAuthEnvironment;
-import auth_environment.view.tabs.ElementTab;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import auth_environment.IAuthEnvironment;
 import auth_environment.Models.LevelOverviewTabModel;
-import auth_environment.Models.UnitView;
-import auth_environment.Models.WaveOverviewTabModel;
 import auth_environment.Models.Interfaces.IAuthModel;
 import auth_environment.Models.Interfaces.ILevelOverviewTabModel;
-import auth_environment.view.UnitPicker;
-import game_engine.TestingEngineWorkspace;
-import game_engine.factories.UnitFactory;
-import game_engine.game_elements.Unit;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.ScrollPane.ScrollBarPolicy;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
-public class LevelOverviewTab extends Tab{
+public class LevelOverviewTab extends Tab {
 	
 	private BorderPane myRoot;
 	private TabPane myTabs;
