7e984d08563bf81eb21c7f46459566cb59f6432c
Andy <ownz.andy@gmail.com>
2016-04-26 03:15:21 -0400

    pushing refactorings

---
+++ src/game_engine/GameEngineStatus.java
@@ -0,0 +1,10 @@
+package game_engine;
+
+public class GameEngineStatus {
+    
+    private int nextWaveTimer;
+    
+    public GameEngineStatus(boolean pause, int score) {
+        
+    }
+}
---
+++ src/game_engine/LevelController.java
@@ -0,0 +1,85 @@
+package game_engine;
+
+import java.util.ArrayList;
+import java.util.List;
+import game_engine.game_elements.Level;
+
+public class LevelController {
+
+    private List<Level> myLevels;
+    private Level myCurrentLevel;
+    private double score;
+    private boolean paused;
+    
+    public LevelController(List<Level> levels, double score, boolean paused) {
+        this.myLevels = levels;
+        this.score = score;
+        this.paused = paused;
+        this.myCurrentLevel = this.myLevels.get(0);
+    }
+    
+    public Level getCurrentLevel() {
+        return myCurrentLevel;
+    }
+    
+    public void setCurrentLevel(Level currentLevel) {
+        this.myCurrentLevel = currentLevel;
+    }
+    
+    public String getGameStatus () {
+        if (myCurrentLevel.getMyLives() <= 0) {
+            return "Waves remaining: " + myCurrentLevel.wavesLeft() + ", Lives remaining: " + "0";
+        }
+        return "Waves remaining: " + myCurrentLevel.wavesLeft() +
+               ", Lives remaining: " + myCurrentLevel.getMyLives();
+    }
+    
+    public int getLives () {
+        return myCurrentLevel.getMyLives();
+    }
+    
+    public void addLevel (Level level) {
+        myLevels.add(level);
+    }
+    
+    public void decrementLives (int lives) {
+        myCurrentLevel.decrementLives(lives);
+    }
+    
+
+    public void playLevel (int levelNumber) {
+        myCurrentLevel = myLevels.get(levelNumber);
+        paused = false;
+    }
+    
+
+    public void continueWaves () {
+        myCurrentLevel.playNextWave();
+        paused = false;
+    }
+    
+    public boolean isGameOver () {
+        return myLevels.get(myLevels.size() - 1).wavesLeft() == 0 || myCurrentLevel.getMyLives() <= 0;
+    }
+    
+    public List<Level> getLevels () {
+        return myLevels;
+    }
+    
+    public double getScore() {
+        return score;
+    }
+    
+    public void setScore(double score) {
+        this.score = score;
+    }
+    
+    public boolean isPaused() {
+        return paused;
+    }
+    
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
+    
+}
