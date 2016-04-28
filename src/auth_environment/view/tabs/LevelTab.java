6fc4a6bc707c8a473cac9d95e8a6390902d0c5a4
Cody <codyli332@gmail.com>
2016-04-28 14:40:11 -0400

    Removed magic numbers from LevelTab class

--- src/auth_environment/view/tabs/LevelTab.java
+++ src/auth_environment/view/tabs/LevelTab.java
@@ -1,5 +1,7 @@
 package auth_environment.view.tabs;
 
+import java.util.ResourceBundle;
+
 import auth_environment.Models.LevelTabModel;
 import auth_environment.Models.Interfaces.IAuthModel;
 import auth_environment.Models.Interfaces.ILevelOverviewTabModel;
@@ -12,11 +14,15 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 
 public class LevelTab extends Tab{
-	
+	private static final String DIMENSIONS_PACKAGE = "auth_environment/properties/dimensions";
+	private ResourceBundle myDimensionsBundle = ResourceBundle.getBundle(DIMENSIONS_PACKAGE);
+
+	private static final String NAMES_PACKAGE = "auth_environment/properties/names";
+	private ResourceBundle myNamesBundle = ResourceBundle.getBundle(NAMES_PACKAGE);
+
 	private BorderPane myBorderPane;
 	private IAuthModel myAuthModel;
 	private String myName; 
-	
 	private LevelTabModel myLevelTabModel;
 	private ILevelOverviewTabModel myLevelOverviewTabModel; 
 	
@@ -52,12 +58,15 @@ public class LevelTab extends Tab{
 	private void createWaveList() { 
 		int index = 0;
 		GridPane newTableInfo = new GridPane();
-		newTableInfo.setPadding(new Insets(15, 12, 15, 12));
-		newTableInfo.setVgap(15);
-		newTableInfo.getColumnConstraints().addAll(new ColumnConstraints(0),new ColumnConstraints(0),new ColumnConstraints(200),new ColumnConstraints(100) );
-		newTableInfo.getRowConstraints().addAll(new RowConstraints(20));
-		newTableInfo.setPrefSize(600, 200);	//TODO: Avoid hard-coded values
-		
+		newTableInfo.setPadding(new Insets(Double.parseDouble(myDimensionsBundle.getString("waveListPaddingTop")), Double.parseDouble(myDimensionsBundle.getString("waveListPaddingRight")), 
+				Double.parseDouble(myDimensionsBundle.getString("waveListPaddingBottom")), Double.parseDouble(myDimensionsBundle.getString("waveListPaddingLeft"))));
+		newTableInfo.setVgap(Double.parseDouble(myDimensionsBundle.getString("waveListVGap")));
+		newTableInfo.getColumnConstraints().addAll(new ColumnConstraints(Double.parseDouble(myDimensionsBundle.getString("waveListConstraintsTop"))), 
+				new ColumnConstraints(Double.parseDouble(myDimensionsBundle.getString("waveListConstraintsRight"))), 
+				new ColumnConstraints(Double.parseDouble(myDimensionsBundle.getString("waveListConstraintsBottom"))), 
+				new ColumnConstraints(Double.parseDouble(myDimensionsBundle.getString("waveListConstraintsLeft"))));
+		newTableInfo.getRowConstraints().addAll(new RowConstraints(Double.parseDouble(myDimensionsBundle.getString("waveListConstraintsRow"))));
+		newTableInfo.setPrefSize(Double.parseDouble(myDimensionsBundle.getString("waveListPrefSizeX")), Double.parseDouble(myDimensionsBundle.getString("waveListPrefSizeY")));	//TODO: Avoid hard-coded values
 		this.myBorderPane.setLeft(newTableInfo);
 		Button dummyWaveButton = new Button();
 		this.addNewWaveSpace(index, newTableInfo, dummyWaveButton);
