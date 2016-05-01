ca08688e59c1060f7fc9d8e6d39a73d909550050
Briguy52 <Briguy52@gmail.com>
2016-04-30 22:42:42 -0400

    Integration: CloudStorage successfully integrated into GameSettingsTab.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,17 @@
 package main;
 
-import game_player.view.PlayerGUI;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import utility.CloudStorageFrontend;
 
 public class Main extends Application {
 
-	private PlayerGUI myView;
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) {
-//		MainView mainView = new MainView(primaryStage);
-		CloudStorageFrontend c = new CloudStorageFrontend(); 
+		MainView mainView = new MainView(primaryStage);
 	}
 	
 }
