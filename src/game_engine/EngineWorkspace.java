6e0e959cba3c1c3c6f80423fc413b7be3b310299
Paul Cruz <pbc8@duke.edu>
2016-04-30 23:27:52 -0400

    fixed merge conflicts

--- src/game_engine/EngineWorkspace.java
+++ src/game_engine/EngineWorkspace.java
@@ -45,129 +45,7 @@ public class EngineWorkspace implements GameEngineInterface {
 	private AIHandler myAIHandler;
 	private AISimulator myAISimulator;
 	private AISearcher myAISearcher;
-<<<<<<< HEAD
 
-
-	public void setUpEngine (IAuthEnvironment data) {
-		myData = data;
-
-		unitsToRemove = new ArrayList<>();
-		myAISearcher = new AISearcher(this);
-		myAIHandler = new AIHandler(this);
-		myAISimulator = new AISimulator(this);
-		waveGoal = data.getWaveGoal();
-		scoreUpdate = data.getScoreUpdate();
-		myBranches = data.getBranches();
-		myAffectors = data.getAffectorFactory().getAffectorLibrary().getAffectors();
-		myAffectors.stream().forEach(a -> a.setWorkspace(this));
-		myCollider = new CollisionDetector(this);
-		myEncapsulator = new EncapsulationDetector(this);
-		myLevelController = new LevelController(data.getLevels(), data.getScore());
-		myLevelController.setCurrentWave(data.getCurrentWaveIndex());
-		List<PlaceValidation> myPlaceValidations = data.getPlaceValidations();
-		myPlaceValidations.stream().forEach(pv -> pv.setEngine(this));
-		myUnitController =
-				new UnitController(data.getPlacedUnits(), myPlaceValidations,
-						data.getStore(), unitsToRemove);
-		myEnemyController = new EnemyController(myLevelController, myUnitController);
-		updateAIBranches();
-	}
-
-	@Override
-	public void update () {
-		Level myCurrentLevel = myLevelController.getCurrentLevel();
-		IStore myStore = myUnitController.getStore();
-		List<Unit> placingUnits = myCurrentLevel.getCurrentWave().getPlacingUnits();
-		myStore.clearBuyableUnits();
-		//     	TODO: store should not be updated here
-		//        placingUnits.stream().forEach(u -> myStore.addBuyableUnit(u, 100));
-		nextWaveTimer++;
-		waveProgression(myCurrentLevel);
-		myUnitController.getUnitType("Projectile").forEach(p -> p.update());
-		myUnitController.getUnitType("Projectile").removeIf(p -> !p.isVisible());
-		myUnitController.getUnitType("Terrain").forEach(t -> t.update());
-		scoreUpdate.updateScore(this, myCurrentLevel);
-		unitsToRemove.stream().forEach(r -> r.setInvisible());
-		unitsToRemove.clear();
-	}
-
-	public void waveProgression (Level myCurrentLevel) {
-		if (!myLevelController.isPaused() && !myLevelController.isGameOver()) {
-			myUnitController.getUnitType("Tower").forEach(t -> t.update());
-			myUnitController.getUnitType("Enemy").forEach(e -> e.update());
-			myCollider.resolveEnemyCollisions(myUnitController.getUnitType("Projectile"));
-			myEncapsulator.resolveEncapsulations(myUnitController.getUnitType("Terrain"));
-			Unit newE = myCurrentLevel.update();
-			if (newE != null) {
-				myUnitController.getPlacedUnits().add(newE);
-			}
-		}
-		if (myCurrentLevel.getNextWave() != null && waveGoal.reachedGoal(this)) {
-			nextWaveTimer = 0;
-			myLevelController.continueWaves();
-		}
-		if (myUnitController.getUnitType("Enemy").size() == 0) {
-			myUnitController.clearProjectiles();
-		}
-	}
-
-	public void saveGame() {
-		Level currentLevel = myLevelController.getCurrentLevel();
-		myData.setCurrentWaveIndex( currentLevel.getWaves().indexOf(currentLevel.getCurrentWave()) );
-		Serializer<IAuthEnvironment> writer = new Serializer<IAuthEnvironment>();
-		writer.saveElement(myData);
-	}
-
-	public List<Affector> getAffectors () {
-		return myAffectors;
-	}
-
-	@Override
-	public List<Branch> getBranches () {
-		return myBranches;
-	}
-
-	@Override
-	public int getNextWaveTimer () {
-		return nextWaveTimer;
-	}
-
-	@Override
-	public void setCursorPosition (double x, double y) {
-		cursorPos = new Position(x, y);
-	}
-
-	public Position getCursorPosition () {
-		return cursorPos;
-	}
-
-	@Override
-	public List<Branch> getBranchesAtPos (Position pos) {
-		return myAIHandler.getBranchesAtPos(pos);
-	}
-
-	@Override
-	public void updateAIBranches () {
-		myAIHandler.updateAIBranches();
-	}
-
-	@Override
-	public UnitController getUnitController () {
-		return myUnitController;
-	}
-
-	@Override
-	public LevelController getLevelController () {
-		return myLevelController;
-	}
-
-	@Override
-	public ILevelDisplayer getLevelDisplay(){
-		return myLevelController;
-	}
-
-	@Override
-=======
 	
 
     public void setUpEngine (IAuthEnvironment data) {
@@ -290,7 +168,6 @@ public class EngineWorkspace implements GameEngineInterface {
     }
     
     @Override
->>>>>>> cea62c131ab3b3174fa2420bf331ebeeedf14503
 	public AIHandler getAIHandler() {
 		return myAIHandler;
 	}
